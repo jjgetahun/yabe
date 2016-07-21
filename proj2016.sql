7952ba8c7caefae06a5d6c949355761ae7a2b3a6
Jon <jjgetahun@gmail.com>
2016-07-21 19:27:34 -0400

    Added passwords to Account table.

--- proj2016.sql
+++ proj2016.sql
@@ -6,13 +6,12 @@ CREATE TABLE Account(
 AccountID int(11) NOT NULL AUTO_INCREMENT,
 Name varchar(255) NOT NULL,
 UserName varchar(255) NOT NULL,
+PassWord varchar(255) NOT NULL,
 DateCreated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(AccountID)
 );
 
---
--- Dumping data for table Person --
-INSERT INTO Account (Name, UserName) VALUES('Elby Basolis', 'ebasolis');
-INSERT INTO Account (Name, UserName) VALUES('Arnold Trakhtenberg', 'atrakh');
-INSERT INTO Account (Name, UserName) VALUES('Jon Getahun', 'jget');
-INSERT INTO Account (Name, UserName) VALUES('New User', 'nuser');
+INSERT INTO Account (Name, UserName, PassWord) VALUES('Elby Basolis', 'ebasolis', 'pass1');
+INSERT INTO Account (Name, UserName, PassWord) VALUES('Arnold Trakhtenberg', 'atrakh', 'pass2');
+INSERT INTO Account (Name, UserName, PassWord) VALUES('Jon Getahun', 'jget', 'pass3');
+INSERT INTO Account (Name, UserName, PassWord) VALUES('New User', 'nuser', 'pass4');
