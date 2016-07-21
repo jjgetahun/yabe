c55a66abbb5ed9b00ac7e2fdb58c8867d4fb9f52
Jon <jjgetahun@gmail.com>
2016-07-21 19:50:51 -0400

    Created Admin table.

--- proj2016.sql
+++ proj2016.sql
@@ -11,7 +11,15 @@ DateCreated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(AccountID)
 );
 
+CREATE TABLE Admin(
+AccountID int(11) NOT NULL,
+PRIMARY KEY(AccountID),
+FOREIGN KEY(AccountID) references Account(AccountID)
+);
+
 INSERT INTO Account (Name, UserName, PassWord) VALUES('Elby Basolis', 'ebasolis', 'pass1');
 INSERT INTO Account (Name, UserName, PassWord) VALUES('Arnold Trakhtenberg', 'atrakh', 'pass2');
 INSERT INTO Account (Name, UserName, PassWord) VALUES('Jon Getahun', 'jget', 'pass3');
 INSERT INTO Account (Name, UserName, PassWord) VALUES('New User', 'nuser', 'pass4');
+
+INSERT INTO Admin VALUES(1);
