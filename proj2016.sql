14b377df32fb539adbbc3f928957a7100b7a5dcc
Jon <jjgetahun@gmail.com>
2016-07-21 19:59:28 -0400

    Created Buyer, Seller, and Customer_Rep tables.

--- proj2016.sql
+++ proj2016.sql
@@ -11,15 +11,42 @@ DateCreated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(AccountID)
 );
 
+INSERT INTO Account (Name, UserName, PassWord) VALUES('Elby Basolis', 'ebasolis', 'pass1');
+INSERT INTO Account (Name, UserName, PassWord) VALUES('Arnold Trakhtenberg', 'atrakh', 'pass2');
+INSERT INTO Account (Name, UserName, PassWord) VALUES('Jon Getahun', 'jget', 'pass3');
+INSERT INTO Account (Name, UserName, PassWord) VALUES('New User', 'nuser', 'pass4');
+INSERT INTO Account (Name, UserName, PassWord) Values('Customer Rep 1', 'crep1', 'pass5');
+
 CREATE TABLE Admin(
 AccountID int(11) NOT NULL,
 PRIMARY KEY(AccountID),
 FOREIGN KEY(AccountID) references Account(AccountID)
 );
 
-INSERT INTO Account (Name, UserName, PassWord) VALUES('Elby Basolis', 'ebasolis', 'pass1');
-INSERT INTO Account (Name, UserName, PassWord) VALUES('Arnold Trakhtenberg', 'atrakh', 'pass2');
-INSERT INTO Account (Name, UserName, PassWord) VALUES('Jon Getahun', 'jget', 'pass3');
-INSERT INTO Account (Name, UserName, PassWord) VALUES('New User', 'nuser', 'pass4');
-
 INSERT INTO Admin VALUES(1);
+INSERT INTO Admin VALUES(2);
+INSERT INTO Admin VALUES(3);
+
+CREATE TABLE Buyer(
+AccountID int(11) NOT NULL,
+PRIMARY KEY(AccountID),
+FOREIGN KEY(AccountID) references Account(AccountID)
+);
+
+INSERT INTO Buyer VALUES(4);
+
+CREATE TABLE Customer_Rep(
+AccountID int(11) NOT NULL,
+PRIMARY KEY(AccountID),
+FOREIGN KEY(AccountID) references Account(AccountID)
+);
+
+INSERT INTO Customer_Rep Values(5);
+
+CREATE TABLE Seller(
+AccountID int(11) NOT NULL,
+PRIMARY KEY(AccountID),
+FOREIGN KEY(AccountID) references Account(AccountID)
+);
+
+INSERT INTO Seller Values(4);
