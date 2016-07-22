e314f43be7a3292c48cdc9d3bc92acfecd087530
Elby Basolis <elby@basolis.com>
2016-07-22 19:10:40 -0400

    Should print row if username/pass to terminal matches, Servlet responds to post request from form submit.

--- src/Servlet.java
+++ src/Servlet.java
@@ -1,15 +1,38 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Servlet extends HttpServlet{
 
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        String initial = config.getInitParameter("initial");
+        DB.init();
+    }
+
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException{
+        //Does login, will probably move this out to JSP and handle with DB
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
 
-        System.out.println(request.getParameter("username"));
-        System.out.println(request.getParameter("password"));
+
+
+        try {
+            Statement statement = DB.conn.createStatement();
+            ResultSet login = statement.executeQuery("SELECT * FROM Account where UserName = \"" + username + "\" AND PassWord = \"" + password);
+                System.out.println(login.getRow());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
